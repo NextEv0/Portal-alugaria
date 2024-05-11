fb2a1b68dbaf977a246af9340caaa54b79260f07
Carlos Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2024-05-11 00:45:18 -0300

    feat: Adição de modulo para conexão com o Cluster Cadmus

---
+++ back/database.py
@@ -0,0 +1,16 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://Carlos_Esteves:mario_elgrably123@cadmus.smz7lcv.mongodb.net/?retryWrites=true&w=majority&appName=Cadmus"
+
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+try:
+    client.admin.command('ping')
+    db = client['Arsenium']
+    User = db['User']
+    Room = db['Room']
+    Reserve = db['Reserve']
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
