00591b321772b4c97322009a888613d3ba7c23c9
Carlos Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2024-05-12 15:40:19 -0300

    feat:feat: __name__ adicionado para qu

--- back/abrirPlanilha.py
+++ back/abrirPlanilha.py
@@ -6,5 +6,6 @@ caminho_arquivo_excel = 'SalasAlugaria.xlsx'
 # Carregar o arquivo Excel em um DataFrame pandas
 tabela_excel = pd.read_excel(caminho_arquivo_excel)
 
-# Exibir a tabela
-print(tabela_excel)
+if __name__ == "__main__":
+    # Exibir a tabela
+    print(tabela_excel)
