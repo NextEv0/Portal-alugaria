6dcc26e8bbf6347a124085dea4a5007e28fe946c
Carlos Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2024-05-14 11:59:29 -0300

    feat: novas funcionalidades adicionadas

--- back/main.py
+++ back/main.py
@@ -1,7 +1,8 @@
-from os import system 
+import pandas as pd
+from os import system
 from database import User, Room, Schedules
 from werkzeug.security import check_password_hash
-from pesquisa import pesquisar_sala, verificar_disponibilidade
+
 
 def login(email: str, password: str):
     response = {'authenticated': False, 'message': None,'user': None}
@@ -23,6 +24,26 @@ def login(email: str, password: str):
     response['message'] = "Acesso concedido"
     response['authenticated'] = True
     return response
+
+
+def room_query(room_name:str):
+    user_df = pd.DataFrame(list(User.find()))
+    user_df['user_id'] = user_df['_id']
+    user_df = user_df.drop(['_id','role','register','account'], axis=1)
+
+    room_df = pd.DataFrame(list(Room.find()))
+    room_df['room_id'] = room_df['_id']
+    room_df = room_df.drop(['_id','floor', 'description'], axis=1)
+
+
+    schedules_df = pd.DataFrame(list(Schedules.find()))
+    query = schedules_df.merge( room_df, on='room_id')
+    query = query.merge(user_df, on='user_id', how='left')
+
+    query["date"] = pd.to_datetime(query["date"]).dt.strftime('%d-%m-%Y')
+    query['start_time'] = pd.to_datetime(query["start_time"]).dt.strftime('%H:%M')
+    query['end_time'] = pd.to_datetime(query["end_time"]).dt.strftime('%H:%M')
+    return query
 
 
 while True:
@@ -56,13 +77,22 @@ while True:
     else:
         print("Comando inválido")
         system("pause")
+
 current_user = response['user']
+
 while True:
     system("cls")
     print("-----------------------------------------")
     print("                                         ")
     print("            [1] PESQUISAR SALA           ")
-    print("            [2]     SAIR                 ")
+    print("            [2] RESERVAR SALA            ")
+    print("            [3]     SAIR                 ")
     print("                                         ")
     print("-----------------------------------------")
     choice = int(input("> "))
+
+    if choice == 1:
+        sala_nome = input("Digite o nome da sala: ")
+        query = room_query(sala_nome)
+        print(query)
+        system("pause")
