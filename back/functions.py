673da28866bcd3c7ae26588ed5ca05e29eaae638
Carlos Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2024-05-28 23:30:13 -0300

    feat: função para alterar a reserva adicionada

--- back/functions.py
+++ back/functions.py
@@ -165,10 +165,69 @@ def cancel_reserve(user_id, room_name, date):
     email = user['account']['email']
     subject = "Confirmação de cancelamento de Reserva de Sala"
     message = f"Olá {user['name']},\n\nSua reserva para a sala {room_name} foi cancelada para o dia {date.strftime('%d-%m-%Y')}.\n\nAtenciosamente,\nEquipe de Reservas"
+    send_email(email, subject, message)
+    
+    return response
+
+
+def update_reserve(user_id, room_name, current_date, new_date):
+    response = {'success': False, 'message': None}
+
+    # Obter dados da sala
+    room_df = room_query(room_name)
+
+    if room_df.empty:
+        response['message'] = "Sala não encontrada"
+        return response
+
+    # Convertendo strings para datetime para comparação
+    current_date = pd.to_datetime(current_date, format='%d-%m-%Y')
+    new_date = pd.to_datetime(new_date, format='%d-%m-%Y')
+
+    # Filtrando para a data específica
+    room_df['date'] = pd.to_datetime(room_df['date'], format='%d-%m-%Y')
+    user_reservations = room_df[(room_df['date'] == current_date) & (room_df['user_id'] == user_id) & (room_df['reserved'] == True)]
+
+    if user_reservations.empty:
+        response['message'] = "Nenhuma reserva encontrada para a data fornecida ou usuário não tem reservas"
+        return response
+
+    # Verificando se o novo dia está disponível nos mesmos horários
+    conflict = False
+    for index, row in user_reservations.iterrows():
+        start_time = row['start_time']
+        end_time = row['end_time']
+        availability = room_df[(room_df['date'] == new_date) & (room_df['reserved'] == True) & 
+                               ((room_df['start_time'] < end_time) & (room_df['end_time'] > start_time))]
+
+        if not availability.empty:
+            conflict = True
+            break
+
+    if conflict:
+        response['message'] = "Novo dia entra em conflito com outra reserva"
+        return response
+
+    # Atualizando o dia da reserva
+    for index, row in user_reservations.iterrows():
+        Schedules.update_one(
+            {'_id': row['_id']},
+            {'$set': {'date': new_date}}
+        )
+
+    response['success'] = True
+    response['message'] = "Data da reserva atualizada com sucesso"
+
+    # Enviando e-mail de confirmação
+    user = User.find_one({'_id': user_id})
+    email = user['account']['email']
+    subject = "Confirmação de Atualização de Reserva de Sala"
+    message = f"Olá {user['name']},\n\nSua reserva para a sala {room_name} foi atualizada para o dia {new_date.strftime('%d-%m-%Y')}.\n\nAtenciosamente,\nEquipe de Reservas"
 
     send_email(email, subject, message)
 
     return response
+
 
 if __name__ == "__main__":
     system("cls")
